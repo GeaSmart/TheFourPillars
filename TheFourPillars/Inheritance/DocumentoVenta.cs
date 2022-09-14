ca6197804848c5e87afbca8074689695e883fd53
Gerson Azabache <gerson_dunk@hotmail.com>
2022-09-13 22:18:23 -0500

    inheritance classes added

---
+++ TheFourPillars/Inheritance/DocumentoVenta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFourPillars
+{
+    public class DocumentoVenta
+    {
+        public int Id { get; set; }
+        public string Serie { get; set; }
+        public string Correlativo { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public double MontoTotal { get; set; }
+    }
+}
---
+++ TheFourPillars/Inheritance/Factura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFourPillars.Inheritance
+{
+    public class Factura : DocumentoVenta
+    {
+        public string RazonSocial { get; set; }
+
+    }
+}
