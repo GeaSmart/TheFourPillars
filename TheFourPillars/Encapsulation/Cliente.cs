cb86f92a9b5e2f228f90c3e3c764cd40fdf479b0
Gerson Azabache <gerson_dunk@hotmail.com>
2022-09-13 22:54:09 -0500

    encapsulation added

---
+++ TheFourPillars/Encapsulation/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFourPillars.Encapsulation
+{
+    public class Cliente
+    {         
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        private double Credito { get; set; }
+
+        public void AumentarCredito(double monto)
+        {
+            this.Credito += monto;
+        }        
+    }
+}
